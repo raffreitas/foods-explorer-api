26a1f454d8b064012f3efe2a7c875b4f2de65755
Rafael Freitas <raffreitasx@gmail.com>
2023-06-25 21:38:53 -0300

    feat(uploads): modulo de upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { UploadsModule } from './uploads/uploads.module';
 
 @Module({
     imports: [
@@ -22,6 +23,7 @@ import { ProductsModule } from './products/products.module';
         UsersModule,
         AuthModule,
         ProductsModule,
+        UploadsModule,
     ],
 })
 export class AppModule {}
